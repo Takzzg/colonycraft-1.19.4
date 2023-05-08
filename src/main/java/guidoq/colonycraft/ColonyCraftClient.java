9dcbabd3a7cacfa3915fbe42cb76a2a39c17ca8c
@Queiroz <guido.queiroz@front.id>
2023-05-08 07:04:25 -0300

    Initial commit

---
+++ src/main/java/guidoq/colonycraft/ColonyCraftClient.java
@@ -0,0 +1,10 @@
+package guidoq.colonycraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ColonyCraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
